92de830a2eb6ed52cfa4f68f941dcdb580e214d9
NelsonPRSousa <nelsonprsousa@gmail.com>
2019-04-06 15:56:59 +0100

    feat: flat list styling

--- src/pages/main.js
+++ src/pages/main.js
@@ -1,12 +1,22 @@
 import React, { Component } from "react";
 import api from "../services/api";
 
-import { View, Text } from "react-native";
+import {
+  View,
+  Text,
+  FlatList,
+  TouchableOpacity,
+  StyleSheet
+} from "react-native";
 import Axios from "axios";
 
 export default class Main extends Component {
   static navigationOptions = {
     title: "JSHunt"
+  };
+
+  state = {
+    docs: []
   };
 
   componentDidMount() {
@@ -18,14 +28,79 @@ export default class Main extends Component {
 
     const { docs } = response.data;
 
-    console.log(docs);
+    this.setState({ docs });
   };
+
+  renderItem = ({ item }) => (
+    <View style={styles.productContainer}>
+      <Text style={styles.productTitle}>{item.title}</Text>
+      <Text style={styles.productDescription}>{item.description}</Text>
+      <TouchableOpacity style={styles.productButton} onPress={() => {}}>
+        <Text style={styles.productButtonText}>See</Text>
+      </TouchableOpacity>
+    </View>
+  );
 
   render() {
     return (
-      <View>
-        <Text>Página Main!</Text>
+      <View style={styles.container}>
+        <FlatList
+          contentContainerStyle={styles.list}
+          data={this.state.docs}
+          keyExtractor={item => item._id}
+          renderItem={this.renderItem}
+        />
       </View>
     );
   }
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#FAFAFA"
+  },
+
+  list: {
+    padding: 20
+  },
+
+  productContainer: {
+    backgroundColor: "#FFF",
+    borderWidth: 1,
+    borderColor: "#DDD",
+    borderRadius: 5,
+    padding: 20,
+    marginBottom: 20
+  },
+
+  productTitle: {
+    fontSize: 18,
+    fontWeight: "bold",
+    color: "#333"
+  },
+
+  productDescription: {
+    fontSize: 16,
+    color: "#999",
+    marginTop: 5,
+    lineHeight: 24
+  },
+
+  productButton: {
+    height: 42,
+    borderRadius: 5,
+    borderWidth: 2,
+    borderColor: "#DA552F",
+    backgroundColor: "transparent",
+    justifyContent: "center",
+    alignItems: "center",
+    marginTop: 10
+  },
+
+  productButtonText: {
+    fontSize: 16,
+    color: "#DA552F",
+    fontWeight: "bold"
+  }
+});
