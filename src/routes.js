e90236354ed5d7ef19700aecaffb6021ef3fb1e9
NelsonPRSousa <nelsonprsousa@gmail.com>
2019-04-06 15:14:29 +0100

    feat: Add header styles

--- src/routes.js
+++ src/routes.js
@@ -2,6 +2,16 @@ import { createStackNavigator } from "react-navigation";
 
 import Main from "./pages/main";
 
-export default createStackNavigator({
-  Main
-});
+export default createStackNavigator(
+  {
+    Main
+  },
+  {
+    navigationOptions: {
+      headerStyle: {
+        backgroundColor: "#DA552F"
+      },
+      headerTintColor: "#FFF"
+    }
+  }
+);
