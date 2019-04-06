fe86a2e01e65ec608ab6dd7d1208dd27af4b0aa8
NelsonPRSousa <nelsonprsousa@gmail.com>
2019-04-06 15:27:17 +0100

    feat: add axios and call api

--- src/pages/main.js
+++ src/pages/main.js
@@ -1,10 +1,24 @@
 import React, { Component } from "react";
+import api from "../services/api";
 
 import { View, Text } from "react-native";
+import Axios from "axios";
 
 export default class Main extends Component {
   static navigationOptions = {
     title: "JSHunt"
+  };
+
+  componentDidMount() {
+    this.loadProduct();
+  }
+
+  loadProduct = async () => {
+    const response = await api.get("/products");
+
+    const { docs } = response.data;
+
+    console.log(docs);
   };
 
   render() {
